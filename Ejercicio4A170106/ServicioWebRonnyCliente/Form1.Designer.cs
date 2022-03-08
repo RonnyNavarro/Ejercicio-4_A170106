8c675b06d90936b6eed62a6e5e1292124fff13b4
Ronny Rodman Navarro Laguna <IPRonnyNavarro@hotmail.com>
2022-03-07 23:27:03 -0600

    Guardado

--- Ejercicio4A170106/ServicioWebRonnyCliente/Form1.Designer.cs
+++ Ejercicio4A170106/ServicioWebRonnyCliente/Form1.Designer.cs
@@ -49,9 +49,10 @@ namespace ServicioWebRonnyCliente
             // 
             // BotonCuadrado
             // 
-            this.BotonCuadrado.Location = new System.Drawing.Point(358, 142);
+            this.BotonCuadrado.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonCuadrado.Location = new System.Drawing.Point(358, 129);
             this.BotonCuadrado.Name = "BotonCuadrado";
-            this.BotonCuadrado.Size = new System.Drawing.Size(153, 23);
+            this.BotonCuadrado.Size = new System.Drawing.Size(153, 40);
             this.BotonCuadrado.TabIndex = 0;
             this.BotonCuadrado.Text = "Calcular Área de Cuadrado";
             this.BotonCuadrado.UseVisualStyleBackColor = true;
@@ -59,9 +60,10 @@ namespace ServicioWebRonnyCliente
             // 
             // BotonTriangulo
             // 
-            this.BotonTriangulo.Location = new System.Drawing.Point(358, 263);
+            this.BotonTriangulo.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonTriangulo.Location = new System.Drawing.Point(358, 246);
             this.BotonTriangulo.Name = "BotonTriangulo";
-            this.BotonTriangulo.Size = new System.Drawing.Size(153, 23);
+            this.BotonTriangulo.Size = new System.Drawing.Size(153, 41);
             this.BotonTriangulo.TabIndex = 1;
             this.BotonTriangulo.Text = "Calcular Área del Triángulo";
             this.BotonTriangulo.UseVisualStyleBackColor = true;
@@ -69,9 +71,10 @@ namespace ServicioWebRonnyCliente
             // 
             // BotonCirculo
             // 
-            this.BotonCirculo.Location = new System.Drawing.Point(358, 376);
+            this.BotonCirculo.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonCirculo.Location = new System.Drawing.Point(358, 353);
             this.BotonCirculo.Name = "BotonCirculo";
-            this.BotonCirculo.Size = new System.Drawing.Size(153, 23);
+            this.BotonCirculo.Size = new System.Drawing.Size(153, 40);
             this.BotonCirculo.TabIndex = 2;
             this.BotonCirculo.Text = "Calcular Área del Círculo";
             this.BotonCirculo.UseVisualStyleBackColor = true;
@@ -80,9 +83,10 @@ namespace ServicioWebRonnyCliente
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(21, 125);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(185, 13);
+            this.label1.Size = new System.Drawing.Size(223, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Ingrese el valor del lado del Cuadrado";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -90,25 +94,27 @@ namespace ServicioWebRonnyCliente
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(19, 236);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(239, 13);
+            this.label2.Size = new System.Drawing.Size(289, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Ingrese los valores de Base y Altura del Triángulo";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(19, 268);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.Size = new System.Drawing.Size(39, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Base:";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // LadoCuadradoTextBox
             // 
-            this.LadoCuadradoTextBox.Location = new System.Drawing.Point(145, 149);
+            this.LadoCuadradoTextBox.Location = new System.Drawing.Point(143, 149);
             this.LadoCuadradoTextBox.Name = "LadoCuadradoTextBox";
             this.LadoCuadradoTextBox.Size = new System.Drawing.Size(100, 20);
             this.LadoCuadradoTextBox.TabIndex = 6;
@@ -123,7 +129,7 @@ namespace ServicioWebRonnyCliente
             // 
             // AlturaTextBox
             // 
-            this.AlturaTextBox.Location = new System.Drawing.Point(218, 265);
+            this.AlturaTextBox.Location = new System.Drawing.Point(223, 265);
             this.AlturaTextBox.Name = "AlturaTextBox";
             this.AlturaTextBox.Size = new System.Drawing.Size(100, 20);
             this.AlturaTextBox.TabIndex = 8;
@@ -131,42 +137,46 @@ namespace ServicioWebRonnyCliente
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(175, 268);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.Size = new System.Drawing.Size(44, 13);
             this.label4.TabIndex = 9;
             this.label4.Text = "Altura:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(18, 345);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(181, 13);
+            this.label5.Size = new System.Drawing.Size(219, 13);
             this.label5.TabIndex = 10;
             this.label5.Text = "Ingrese el valor del Radio del Círculo";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(21, 152);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(98, 13);
+            this.label6.Size = new System.Drawing.Size(115, 13);
             this.label6.TabIndex = 11;
             this.label6.Text = "Lado de Cuadrado:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(21, 376);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.Size = new System.Drawing.Size(44, 13);
             this.label7.TabIndex = 12;
             this.label7.Text = "Radio:";
             // 
             // RadioTextBox
             // 
-            this.RadioTextBox.Location = new System.Drawing.Point(65, 373);
+            this.RadioTextBox.Location = new System.Drawing.Point(67, 373);
             this.RadioTextBox.Name = "RadioTextBox";
             this.RadioTextBox.Size = new System.Drawing.Size(100, 20);
             this.RadioTextBox.TabIndex = 13;
